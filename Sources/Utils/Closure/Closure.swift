0d0ac51e88aa30329367b7ce1ea1c188b9942b6b
Yan Boyko <yanboyko21@gmail.com>
2025-02-28 22:01:41 +0300

    add sendable conformance to closure

--- Sources/Utils/Closure/Closure.swift
+++ Sources/Utils/Closure/Closure.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-public enum Closure: Sendable {
-    public typealias Void = (() -> Swift.Void)
-    public typealias Boolean = ((Swift.Bool) -> Swift.Void)
-    public typealias Int = ((Swift.Int) -> Swift.Void)
-    public typealias UInt = ((Swift.UInt) -> Swift.Void)
-    public typealias Float = ((Swift.Float) -> Swift.Void)
-    public typealias Double = ((Swift.Double) -> Swift.Void)
-    public typealias String = ((Swift.String) -> Swift.Void)
-    public typealias URL = ((Foundation.URL) -> Swift.Void)
+public enum Closure {
+    public typealias Void = (@Sendable () -> Swift.Void)
+    public typealias Boolean = (@Sendable (Swift.Bool) -> Swift.Void)
+    public typealias Int = (@Sendable (Swift.Int) -> Swift.Void)
+    public typealias UInt = (@Sendable (Swift.UInt) -> Swift.Void)
+    public typealias Float = (@Sendable (Swift.Float) -> Swift.Void)
+    public typealias Double = (@Sendable (Swift.Double) -> Swift.Void)
+    public typealias String = (@Sendable (Swift.String) -> Swift.Void)
+    public typealias URL = (@Sendable (Foundation.URL) -> Swift.Void)
     // swiftlint: disable syntactic_sugar
-    public typealias Array<T> = (([T]) -> Swift.Void)
-    public typealias Generic<T> = ((T) -> Swift.Void)
+    public typealias Array<T> = (@Sendable ([T]) -> Swift.Void) where T: Sendable
+    public typealias Generic<T> = (@Sendable (T) -> Swift.Void) where T: Sendable
 }
